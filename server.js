23eaacc2f0e5db6a8aa409d47ac7d489136e43d2
idham kholid <idhamkholid965@gmail.com>
2020-05-02 17:12:23 +0700

    versi 1.0

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongo
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// use Routes
+app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
